a56fbbdcc0a37e83f8ee2556d4e63bc634e4a908
Eric Saxby <sax@livinginthepast.org>
2015-09-10 13:32:22 -0700

    Update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -1,5 +1,5 @@
 # http://us-east.manta.joyent.com/bahamas10/public/chef-standalone/index.html
-pkgin in -y gmake gcc47
+pkgin -y in gmake gcc47
 cd / \
   && curl http://us-east.manta.joyent.com/bahamas10/public/chef-standalone/smartos-latest-x64.tar.gz \
   | gzcat | \
