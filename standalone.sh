3b0c9e42974e4064651ebd9fe79c5064ebf42e74
Eric Saxby <sax@livinginthepast.org>
2015-09-10 15:52:47 -0700

    standalone links rubies in /opt/chef/embedded

--- standalone.sh
+++ standalone.sh
@@ -4,3 +4,8 @@ cd / \
   && curl http://us-east.manta.joyent.com/bahamas10/public/chef-standalone/smartos-latest-x64.tar.gz \
   | gzcat | \
   tar -xf -
+
+# required by test-kitchen
+mkdir -p /opt/chef/embedded/bin
+ln -s /opt/chef/bin/ruby /opt/chef/embedded/bin/ruby
+ln -s /opt/chef/bin/gem /opt/chef/embedded/bin/gem
